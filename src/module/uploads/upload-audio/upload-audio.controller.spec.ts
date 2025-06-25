08fd02f02d76cdb8286f9d3cd43c97a86b742782
nattanjunior <fnatanieljunior@gmail.com>
2025-06-25 18:20:31 -0300

    test(controller): testes unitários do controller de upload de áudio

--- src/module/uploads/upload-audio/upload-audio.controller.spec.ts
+++ src/module/uploads/upload-audio/upload-audio.controller.spec.ts
@@ -1,5 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UploadAudioController } from './upload-audio.controller';
+import { beforeEach, describe, expect, it } from '@jest/globals';
 
 describe('UploadAudioController', () => {
   let controller: UploadAudioController;
