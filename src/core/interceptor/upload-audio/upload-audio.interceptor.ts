767fc78c3a7b9e0d95cb4fed9bc4825fac9e8a7d
nattanjunior <fnatanieljunior@gmail.com>
2025-06-25 18:19:58 -0300

    refactor(interceptor): ajustes e validações no interceptor de upload de áudio

--- src/core/interceptor/upload-audio/upload-audio.interceptor.ts
+++ src/core/interceptor/upload-audio/upload-audio.interceptor.ts
@@ -1,9 +1,34 @@
-import { CallHandler, ExecutionContext, Injectable, NestInterceptor } from '@nestjs/common';
+import { CallHandler, ExecutionContext, HttpException, HttpStatus, Injectable, NestInterceptor } from '@nestjs/common';
 import { Observable } from 'rxjs';
+
+const fileSize = 1_848_576 * 5 // 5mb
 
 @Injectable()
 export class UploadAudioInterceptor implements NestInterceptor {
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    const request = context.switchToHttp().getRequest();
+
+    if (!request.file) {
+      throw new HttpException('Arquivo não enviado', HttpStatus.BAD_REQUEST);
+    }
+
+    const file: Express.Multer.File = request.file;
+
+    if (file.mimetype !== 'audio/mpeg') {
+      throw new HttpException(
+        'Arquivo de audio invalido.',
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+
+    
+    if (file.size !== fileSize) {
+      throw new HttpException(
+        'Arquivo maior que o suportado (5mb)',
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+
     return next.handle();
   }
 }
