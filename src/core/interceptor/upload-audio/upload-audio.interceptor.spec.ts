0046c4515fcff6c163137ea65185ea637a305ef5
nattanjunior <fnatanieljunior@gmail.com>
2025-06-25 16:04:35 -0300

    chore(core): adiciona estrutura inicial do diretório core
    - Cria diretório core para centralizar funcionalidades e utilitários compartilhados do projeto.
    - Prepara o projeto para melhor organização e escalabilidade.

---
+++ src/core/interceptor/upload-audio/upload-audio.interceptor.spec.ts
@@ -0,0 +1,7 @@
+import { UploadAudioInterceptor } from './upload-audio.interceptor';
+
+describe('UploadAudioInterceptor', () => {
+  it('should be defined', () => {
+    expect(new UploadAudioInterceptor()).toBeDefined();
+  });
+});
