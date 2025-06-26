e2681b48f57b2be27212073d1228f8f39b559619
nattanjunior <fnatanieljunior@gmail.com>
2025-06-25 21:47:18 -0300

    feat(swagger): configura documentação Swagger na aplicação principal

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,19 @@
 import 'dotenv/config'
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Docs')
+    .setDescription('docs of api-upload')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, documentFactory);
+
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.PORT ?? 3000);
 }
