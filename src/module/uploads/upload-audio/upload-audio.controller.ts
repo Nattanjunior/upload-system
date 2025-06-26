cb3c4e58135c5fd3a7f93e918af89ab5fc43af6d
nattanjunior <fnatanieljunior@gmail.com>
2025-06-25 21:47:23 -0300

    feat(controller): adiciona decorators do Swagger para documentação do endpoint

--- src/module/uploads/upload-audio/upload-audio.controller.ts
+++ src/module/uploads/upload-audio/upload-audio.controller.ts
@@ -8,6 +8,7 @@ import {
 import { UploadAudioService } from './upload-audio.service';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { UploadAudioInterceptor } from '../../../core/interceptor/upload-audio/upload-audio.interceptor';
+import { ApiBody, ApiConsumes } from '@nestjs/swagger';
 
 @Controller('upload/audio')
 export class UploadAudioController {
@@ -18,6 +19,13 @@ export class UploadAudioController {
   @HttpCode(201)
   @UseInterceptors(UploadAudioInterceptor)
   @UseInterceptors(FileInterceptor('file'))
+  @ApiConsumes('multipart/form-data')
+  @ApiBody({
+    schema: {
+      type: 'object',
+      properties: { file: { type: 'string', format: 'binary' } }
+    }
+  })
   async upload(@UploadedFile() file: Express.Multer.File) {
     return this.UploadAudioService.execute(file);
   }
